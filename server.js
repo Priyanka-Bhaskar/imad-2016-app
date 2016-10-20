f8f2d84f8d0b30ca9f4af6cfba6fa87a4d1597de
Priyanka-Bhaskar <priyankavbhaskar@gmail.com>
2016-10-21 00:25:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.get('/ui/imad1.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'imad1.png'));
 });
 
+app.get('/ui/imad2.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'imad2.png'));
+});
+app.get('/ui/imad3.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'imad3.png'));
+});
+
+app.get('/ui/imad4.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'imad4.png'));
+});
+
+app.get('/ui/imad5.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'imad5.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
